4d56816acdbb026f153e434e48b756245516033f
HanYw12 <67331821+HanYw12@users.noreply.github.com>
2022-04-01 10:00:21 -0500

    primer Git k vale jajaja

--- app/src/main/java/com/example/finak/ActivityTable.kt
+++ app/src/main/java/com/example/finak/ActivityTable.kt
@@ -1,14 +1,13 @@
 package com.example.finak
 
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.EditText
-import android.widget.TableLayout
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
@@ -18,68 +17,111 @@ import org.json.JSONException
 
 
 class ActivityTable : AppCompatActivity() {
-    private var txtNombre:EditText?=null
-    private var txtIngreso: EditText?=null
-    private var txtGastos:EditText?=null
-    private var tbMovimientos:TableLayout?=null
+    private var txtNombre: EditText? = null
+    private var txtIngreso: EditText? = null
+    private var txtGastos: EditText? = null
+    private var tbMovimientos: TableLayout? = null
+
+    private var tbCalculos2: TableLayout? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_table)
-        txtNombre=findViewById(R.id.colNombre)
-        txtIngreso=findViewById(R.id.colIngresos)
-        txtGastos=findViewById(R.id.colGastos)
-        tbMovimientos=findViewById(R.id.tbMovimientos)
+        txtNombre = findViewById(R.id.colNombre)
+        txtIngreso = findViewById(R.id.colIngresos)
+        txtGastos = findViewById(R.id.colGastos)
+        tbMovimientos = findViewById(R.id.tbMovimientos)
         tbMovimientos?.removeAllViews()
-        var queue=Volley.newRequestQueue(this)
-        var url="http://192.168.3.72/APIFinAK/registro.php?id=1"
-        var jsonObjectRequest =  JsonObjectRequest(
+        var queue = Volley.newRequestQueue(this)
+        var url = "http://192.168.3.72/APIFinAK/registro.php?id=1"
+        var jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET,
             url,
             null,
-            Response.Listener{ response ->
+            Response.Listener { response ->
                 try {
-                    var jsonArray:JSONArray =response.getJSONArray("data")
-                    for(i in 0 until jsonArray.length() ){
+                    var jsonArray: JSONArray = response.getJSONArray("data")
+                    for (i in 0 until jsonArray.length()) {
                         var jsonObject = jsonArray.getJSONObject(i)
-                        val registro = LayoutInflater.from(this).inflate(R.layout.table_row_ak, null, false)
+                        val registro =
+                            LayoutInflater.from(this).inflate(R.layout.table_row_ak, null, false)
                         val colNombre = registro.findViewById<View>(R.id.colNombre) as TextView
-                        val colIngreso =registro.findViewById<View>(R.id.colIngresos) as TextView
+                        val colIngreso = registro.findViewById<View>(R.id.colIngresos) as TextView
                         val colGasto = registro.findViewById<View>(R.id.colGastos) as TextView
                         val colEditar = registro.findViewById<View>(R.id.colEditar)
                         val colBorrar = registro.findViewById<View>(R.id.colBorrar)
-                        if (i == 0){
-                            colNombre.text="Nombre"
-                            colIngreso.text="Ingreso"
-                            colGasto.text="Gasto"
 
-                            tbMovimientos?.addView(registro)
-                        }else {
-                            if (jsonObject.getString("MovTipo") == "Ingreso") {
-                                colIngreso.text = jsonObject.getString("MovCantidad")
-                                colGasto.text = null
-                            } else {
-                                colIngreso.text = null
-                                colGasto.text = jsonObject.getString("MovCantidad")
-                            }
-                            colNombre.text = jsonObject.getString("MovNombre")
-                            colEditar.id = jsonObject.getString("MovId").toInt()
-                            colBorrar.id = jsonObject.getString("MovId").toInt()
-                            tbMovimientos?.addView(registro)
+                        if (jsonObject.getString("MovTipo") == "Ingreso") {
+                            colIngreso.text = jsonObject.getString("MovCantidad")
+                            colGasto.text = null
+                        } else {
+                            colIngreso.text = null
+                            colGasto.text = jsonObject.getString("MovCantidad")
                         }
+                        colNombre.text = jsonObject.getString("MovNombre")
+                        colEditar.id = jsonObject.getString("MovId").toInt()
+                        colBorrar.id = jsonObject.getString("MovId").toInt()
+                        tbMovimientos?.addView(registro)
+
                     }
-                }catch (e: JSONException){
+                } catch (e: JSONException) {
                     e.printStackTrace()
-                    Toast.makeText(this,"Error", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
                 }
-            },Response.ErrorListener { error ->
+            }, Response.ErrorListener { error ->
             })
         queue.add(jsonObjectRequest)
     }
-    fun clickbtnEditar(view: View){
-        Toast.makeText(this,view.id.toString(),Toast.LENGTH_LONG).show()
+
+    fun clickbtnEditar(view: View) {
+//        Toast.makeText(this, view.id.toString(), Toast.LENGTH_LONG).show()
+        val id =view.id.toString()
+        val intent: Intent = Intent(this,MainActivity::class.java)
+        intent.putExtra("id",id)
+        startActivity(intent)
+        }
+
+
+    fun clickbtnBorrar(view: View) {
+        Toast.makeText(this, view.id.toString(), Toast.LENGTH_LONG).show()
     }
-    fun clickbtnBorrar(view: View){
-        Toast.makeText(this,view.id.toString(),Toast.LENGTH_LONG).show()
+
+    fun clickbtnCalcular(view: View) {
+        val Calcular = findViewById<Button>(R.id.Calcular)
+        Calcular.visibility = View.GONE
+        tbCalculos2 = findViewById(R.id.tbCalculos)
+        tbCalculos2?.removeAllViews()
+        var queue1 = Volley.newRequestQueue(this)
+        //var queue2=Volley.newRequestQueue(this)
+        var url1 = "http://192.168.3.72/APIFinAK/gasto.php?id=1"
+        val jsonObjectRequest1 = JsonObjectRequest(Request.Method.GET, url1, null,
+            Response.Listener { response ->
+                var jsonArray: JSONArray = response.getJSONArray("data")
+                var jsonObject = jsonArray.getJSONObject(1)
+                var jsonObject2 = jsonArray.getJSONObject(0)
+                val registro2 =
+                    LayoutInflater.from(this).inflate(R.layout.table_row_calculos, null, false)
+                val colCalIng = registro2.findViewById<View>(R.id.colCalIngText) as TextView
+                val colCalGas = registro2.findViewById<View>(R.id.colCalGasText) as TextView
+                colCalIng.text = jsonObject.getString("Suma")
+                colCalGas.text = jsonObject2.getString("Suma")
+                val colIng = jsonObject.getString("MovTipo")
+                val colGas = jsonObject2.getString("MovTipo")
+
+                if (colIng.toString() == "Ingreso") {
+                    if (colGas.toString() == "Gasto") {
+                        colCalIng.text = jsonObject.getString("Suma")
+                        colCalGas.text = jsonObject2.getString("Suma")
+                        tbCalculos2?.addView(registro2)
+                    }
+                } else {
+                    colCalIng.text = jsonObject2.getString("Suma")
+                    colCalGas.text = jsonObject.getString("Suma")
+
+                    tbCalculos2?.addView(registro2)
+
+                }
+            }, Response.ErrorListener { error -> })
+        queue1.add(jsonObjectRequest1)
     }
 
 }
--- app/src/main/java/com/example/finak/MainActivity.kt
+++ app/src/main/java/com/example/finak/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.widget.*
 import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import kotlin.collections.HashMap
@@ -15,29 +16,125 @@ import kotlin.collections.HashMap
 
 class MainActivity : AppCompatActivity(){
 
+    var txtMovimiento : EditText?=null
+    var txtCantidad : EditText?=null
+    var txtTipoMovimiento : Spinner?=null
+    var txtAño : EditText?=null
+    var txtMes : EditText?=null
+    var cad : CheckBox?=null
+    var spiTipo : Spinner?=null
+    var id : String?=null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        id = intent.getStringExtra("id")
+        if (id!=null){
+            val btnInsertar=findViewById<Button>(R.id.Insertar)
+            btnInsertar.visibility= View.GONE
+            val btnEditar = findViewById<Button>(R.id.Editar)
+            btnEditar.visibility= View.VISIBLE
+            val btnInsNew = findViewById<Button>(R.id.InsNew)
+            btnInsNew.visibility= View.VISIBLE
+            txtMovimiento=findViewById(R.id.Movimiento)
+            txtCantidad=findViewById(R.id.Cantidad)
+            cad=findViewById(R.id.Fijos)
+            val queue = Volley.newRequestQueue(this)
+            val url = "http://192.168.3.72/APIFinAK/ingreso.php?MovId=$id"
+            val jsonObjectRequest = JsonObjectRequest(Request.Method.GET
+            ,url,null,Response.Listener{
+                response->
+                    txtMovimiento?.setText(response.getString("MovNombre"))
+                    txtCantidad?.setText(response.getString("MovCantidad"))
+//                    datos Spinner
+                    val Spin = response.getString("MovTipo")
+                    if(Spin=="Gasto"){
+                        spiTipo = findViewById(R.id.TipoMovimientoDown)
+                        spiTipo?.setSelection(1)
+                    }else{
+                        spiTipo?.setSelection(0)
+                    }
+                    var cad2 = response.getString("MovFijo")
+                    cad?.isChecked = cad2.toString()=="1"
+                    val Año = findViewById<TextView>(R.id.Año)
+                    val Mes = findViewById<TextView>(R.id.Mes)
+                    if (cad2.toString()=="1" ){
+                        Año.visibility= View.VISIBLE
+                        Mes.visibility= View.VISIBLE
+                    }else {
+                        Año.visibility = View.GONE
+                        Mes.visibility = View.GONE
+                    }
+
+            },Response.ErrorListener{
+                error ->
+            })
+            queue.add(jsonObjectRequest)
+        }
+
     }
 //cambio de pantalla
     fun clickBtnTabla(view: View){
         val intent:Intent = Intent(this,ActivityTable::class.java)
         startActivity(intent)
     }
+    fun clickbtnEditar(view: View) {
+        id = intent.getStringExtra("id")
 
+        Toast.makeText(this, id.toString(), Toast.LENGTH_LONG).show()
+
+        val url="http://192.168.3.72/APIFinAK/editar.php"
+        val queue = Volley.newRequestQueue(this)
+        txtMovimiento = findViewById<TextView>(R.id.Movimiento) as EditText?
+
+//CheckBox
+        cad = findViewById(R.id.Fijos)
+        val cad2 = cad.toString()
+        txtCantidad = findViewById<TextView>(R.id.Cantidad) as EditText?
+//sacar de spinner informacion 1
+        txtTipoMovimiento= findViewById(R.id.TipoMovimientoDown)
+        txtAño = findViewById<TextView>(R.id.Año) as EditText?
+        txtMes = findViewById<TextView>(R.id.Mes) as EditText?
+        var resultadoPost = object : StringRequest(Request.Method.POST,url,
+            Response.Listener{response ->
+                Toast.makeText(this, "Movimiento ingresado",Toast.LENGTH_SHORT).show()
+            }, Response.ErrorListener { error ->
+                Toast.makeText(this,"error $error",Toast.LENGTH_LONG).show()
+            }){
+            override fun getParams(): MutableMap<String, String>? {
+                val parametros = HashMap<String, String>()
+                parametros.put("nombre",txtMovimiento?.text.toString())
+                //sacar de spinner informacion 2
+                parametros.put("tipo",txtTipoMovimiento?.selectedItem.toString())
+                parametros.put("cantidad",txtCantidad?.text.toString())
+                //sacar Checkbox
+                parametros.put("fijo", cad2)
+                parametros.put("año",txtAño?.text.toString())
+                parametros.put("mes",txtMes?.text.toString())
+                parametros.put("id",txtMes?.text.toString())
+
+                return parametros
+            }
+        }
+        queue.add(resultadoPost)
+
+
+
+    }
     fun clickBtnInsertar (view: View){
         val url="http://192.168.3.72/APIFinAK/insertar.php"
         val queue = Volley.newRequestQueue(this)
-        val txtMovimiento =findViewById<TextView>(R.id.Movimiento)
+        txtMovimiento = findViewById<TextView>(R.id.Movimiento) as EditText?
+
 //CheckBox
-        val cad = findViewById<CheckBox>(R.id.Fijos).isChecked
+        cad = findViewById(R.id.Fijos)
         val cad2 = cad.toString()
-        Toast.makeText(this, cad2, Toast.LENGTH_SHORT).show()
-        val txtCantidad =findViewById<TextView>(R.id.Cantidad)
+        txtCantidad = findViewById<TextView>(R.id.Cantidad) as EditText?
 //sacar de spinner informacion 1
-        val txtTipoMovimiento= findViewById<Spinner>(R.id.TipoMovimientoDown)
-        val txtAño = findViewById<TextView>(R.id.Año)
-        val txtMes = findViewById<TextView>(R.id.Mes)
+        txtTipoMovimiento= findViewById(R.id.TipoMovimientoDown)
+        txtAño = findViewById<TextView>(R.id.Año) as EditText?
+        txtMes = findViewById<TextView>(R.id.Mes) as EditText?
         var resultadoPost = object : StringRequest(Request.Method.POST,url,
             Response.Listener{response ->
                 Toast.makeText(this, "Movimiento ingresado",Toast.LENGTH_SHORT).show()
@@ -73,5 +170,14 @@ class MainActivity : AppCompatActivity(){
             Año.visibility = View.GONE
             Mes.visibility = View.GONE
         }
+    }
+    fun clickBtnInsertarNew(view: View){
+        var btnInsertar=findViewById<Button>(R.id.Insertar)
+        btnInsertar.visibility= View.VISIBLE
+        var btnEditar = findViewById<Button>(R.id.Editar)
+        btnEditar.visibility= View.GONE
+        var btnInsNew = findViewById<Button>(R.id.InsNew)
+        btnInsNew.visibility= View.GONE
+
     }
 }
